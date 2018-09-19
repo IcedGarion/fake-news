9cc12887cc332791a5a7520e9a7df70ae2a9cc8f
Garion <garion.musetta@studenti.unimi.it>
2018-09-19 02:53:57 +0200

    Counts

--- python/features_exploration.py
+++ python/features_exploration.py
@@ -24,15 +24,29 @@ for name, obj in inspect.getmembers(Features):	# se importi una classe in Featur
 	applying features to the dataset '''
 # results: { feature x: { unreliables: [ 0.1, 0.2, 0.3, ...], reliables: [ 0,4, 0.5, 0.6, ...] } }
 results = { str(feat): { dataset.namesmap["fake_label"]: [], dataset.namesmap["nonfake_label"]: []} for feat in feats }
+reliable_count = 0
+unreliable_count = 0
+unknown_count = 0
+i = 0
 try:
 	print("Processing records...")
-	for i, record in enumerate(dataset):
-		print("Record {}... ({})".format(i, record[dataset.namesmap["fake_attribute"]]))
+	for record in dataset:
+		# record: reliable or not?
+		reliable = record[dataset.namesmap["fake_attribute"]]
+		print("Record {}... ({})".format(i, reliable))
+		if reliable == dataset.namesmap["nonfake_label"]:
+			reliable_count += 1
+		elif reliable == dataset.namesmap["fake_label"]:
+			unreliable_count += 1
+		else:
+			unknown_count += 1
+
+		# applying feature's method "score" and passing the dataset's records attributes names (namesmap)
 		for feature in feats:
-			# applying feature's method "score" and passing the dataset's records attributes names (namesmap)
 			results[str(feature)][record[dataset.namesmap["fake_attribute"]]].append(feature.score(record, dataset.namesmap))
 
 		# BREAK PER TROPPI RECORDS
+		i += 1
 		if i >= max_records:
 			break
 except KeyboardInterrupt:
@@ -44,6 +58,6 @@ print("Done! Printing results\n")
 formt = "{:^" + str(output_tab_width) + ".5}"
 print("\t\t" + "\t\t".join([str(f) for f in feats]))
 # applying feature's method "mean"
-print("unreliable\t" + "\t".join([formt.format(str(f.mean(results[str(f)][dataset.namesmap["fake_label"]]))) for f in feats]))
-print("reliable\t" + "\t".join([formt.format(str(f.mean(results[str(f)][dataset.namesmap["nonfake_label"]]))) for f in feats]))
-
+print("(" + str(unreliable_count) + ") unreliable\t" + "\t".join([formt.format(str(f.mean(results[str(f)][dataset.namesmap["fake_label"]]))) for f in feats]))
+print("(" + str(reliable_count) + ") reliable\t" + "\t".join([formt.format(str(f.mean(results[str(f)][dataset.namesmap["nonfake_label"]]))) for f in feats]))
+print("\n" + str(unknown_count) + " unknown\n" + str(i+1) + " tot records")
