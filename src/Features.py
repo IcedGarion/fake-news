716fdbff3ba333bec6e4ddd778a0351f56a0121a
Garion <garion.musetta@studenti.unimi.it>
2018-09-25 16:21:09 +0200

    Scatterplot legend bug fixed; minor fixes

--- src/Features.py
+++ src/Features.py
@@ -51,54 +51,54 @@ class Feature:
 
 
 
-#class morfological_complexity(Feature):
-#	''' stanford parser: complexity of the sentences parsing tree '''
-#
-#	def __init__(self, namesmap):
-#		self.namesmap = namesmap
-#		# stanford parser setup
-#		parser_base_dir = "parser/stanford-parser-full-2018-02-27"
-#		modelpath = model_path=parser_base_dir + "/englishPCFG.ser.gz"
-#		os.environ['CLASSPATH'] = parser_base_dir
-#		os.environ['STANFORD_PARSER'] = parser_base_dir
-#		os.environ['STANFORD_MODELS'] = parser_base_dir
-#		self.parser = stanford.StanfordParser(model_path=modelpath, encoding='utf8')
-#		# parameters: sentences must be split for raw_parse_sents
-#		self.endsentence_punctuation_regex = "[.?!]+"
-#		self.min_text = 3
-#
-#	def score(self, record):
-#		# converts to unicode-utf8 for compatibility problems with python2 (& splits sentences)
-#		sentences = [sentence for sentence in \
-#			re.split(self.endsentence_punctuation_regex, unicode(record[self.namesmap["text_attribute"]], "utf-8")) \
-#			if len(sentence) >= self.min_text]
-#		# parse tree of every sentence in the record
-#		parse_tree = self.parser.raw_parse_sents(sentences)
-#		# reduce sentences heights with max
-#		return functools.reduce(lambda x, y: max(x, y), itertools.chain(*[[sentence.height() for sentence in line] for line in parse_tree]))
-#
-#	def __str__(self):
-#		return "morfological_complexity"
-#
-#
-#
-
-class lexical_variety(Feature):
-	''' pattern.metrics.ttr: average percentage of unique words (types)
-		for each n successive words (tokens) in the text.
-		https://www.clips.uantwerpen.be/pages/pattern-metrics'''
+class morfological_complexity(Feature):
+	''' stanford parser: complexity of the sentences parsing tree '''
 
 	def __init__(self, namesmap):
-		# min text length
-		self.min_text = 3
 		self.namesmap = namesmap
+		# stanford parser setup
+		parser_base_dir = ".." + os.sep + "parser" + os.sep + "stanford-parser-full-2018-02-27"
+		modelpath = parser_base_dir + os.sep + "englishPCFG.ser.gz"
+		os.environ['CLASSPATH'] = parser_base_dir
+		os.environ['STANFORD_PARSER'] = parser_base_dir
+		os.environ['STANFORD_MODELS'] = parser_base_dir
+		self.parser = stanford.StanfordParser(model_path=modelpath, encoding='utf8')
+		# parameters: sentences must be split for raw_parse_sents
+		self.endsentence_punctuation_regex = "[.?!]+"
+		self.min_text = 3
 
 	def score(self, record):
-		# applies ttr function to "text", with n the number of distinct tokens (use a tokenizer ?)
-		# checks for the text not being a string or "empty"
-		return ttr(record["text"], n=len(re.split("\W+", record[self.namesmap["text_attribute"]]))) \
-			if isinstance(record["text"], str) and len(record[self.namesmap["text_attribute"]]) > self.min_text \
-			else 0
+		# converts to unicode-utf8 for compatibility problems with python2 (& splits sentences)
+		sentences = [sentence for sentence in \
+			re.split(self.endsentence_punctuation_regex, unicode(record[self.namesmap["text_attribute"]], "utf-8")) \
+		if len(sentence) >= self.min_text]
+		# parse tree of every sentence in the record
+		parse_tree = self.parser.raw_parse_sents(sentences)
+		# reduce sentences heights with max
+		return functools.reduce(lambda x, y: max(x, y), itertools.chain(*[[sentence.height() for sentence in line] for line in parse_tree]))
 
 	def __str__(self):
-		return "lexical_variety"
+		return "morfological_complexity"
+
+
+
+
+#class lexical_variety(Feature):
+#	''' pattern.metrics.ttr: average percentage of unique words (types)
+#		for each n successive words (tokens) in the text.
+#		https://www.clips.uantwerpen.be/pages/pattern-metrics'''
+#
+#	def __init__(self, namesmap):
+#		# min text length
+#		self.min_text = 3
+#		self.namesmap = namesmap
+#
+#	def score(self, record):
+#		# applies ttr function to "text", with n the number of distinct tokens (use a tokenizer ?)
+#		# checks for the text not being a string or "empty"
+#		return ttr(record["text"], n=len(re.split("\W+", record[self.namesmap["text_attribute"]]))) \
+#			if isinstance(record["text"], str) and len(record[self.namesmap["text_attribute"]]) > self.min_text \
+#			else 0
+#
+#	def __str__(self):
+#		return "lexical_variety"
